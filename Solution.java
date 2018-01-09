c350fa243433ba5dbb6ab303cafa49acd22a22ec
yang112196 <32599960+yang112196@users.noreply.github.com>
2018-01-09 11:54:38 -0600

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,38 @@
+package Solution;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String twoStrings(String s1, String s2){
+        String result = "";
+        
+        for(int i = 0; i<s1.length(); ++i){
+            for(int j = 0; j<s2.length();++j){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    result = "YES";
+                    break;
+                }
+                else{
+                    result = "NO";
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s1 = in.next();
+            String s2 = in.next();
+            String result = twoStrings(s1, s2);
+            System.out.println(result);
+        }
+    }
+}
